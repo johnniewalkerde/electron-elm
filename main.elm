271bfdfc0ccb241fa31ce6e2d3268af6a03db83c
Johannes Körner <johnniewalker.de@gmail.com>
2017-08-22 20:20:28 +0200

    counter

--- main.elm
+++ main.elm
@@ -1,5 +1,24 @@
 module Main exposing (..)
-import Html
-main : Html.Html msg
+import Html exposing (beginnerProgram, div, button, text)
+import Html.Events exposing (onClick)
+
 main =
-    Html.text "WOW"
+  beginnerProgram { model = 0, view = view, update = update }
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+      
